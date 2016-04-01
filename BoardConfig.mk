1cf0d93c413abe5e168ab675f4a64f23ece43fd3
DERRAZ Abdessamad <derraz.abdessamad@gmail.com>
2016-04-01 13:45:50 +0200

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,213 @@
+#
+# Copyright (C) 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := krait
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Krait optimizations
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_KRAIT_BIONIC_PLDOFFS := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH := 64
+TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
+# Kernel
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom loglevel=1 user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_CUSTOM_BOOTIMG_MK := device/iuni/u3/mkbootimg.mk
+TARGET_KERNEL_ARCH := arm
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0008000 --ramdisk_offset 0x01000000 --tags_offset 0x0000100
+
+# global
+TARGET_SPECIFIC_HEADER_PATH := device/iuni/u3/include
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP -DQCOM_MEDIA_DISABLE_BUFFER_SIZE_CHECK
+
+# Display
+TARGET_QCOM_DISPLAY_VARIANT := caf-bfam
+USE_OPENGL_RENDERER := true
+TARGET_USES_ION := true
+TARGET_USES_C2D_COMPOSITION := true
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Time Daemon
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Media
+TARGET_QCOM_MEDIA_VARIANT := caf-bfam
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# audio
+BOARD_USES_ALSA_AUDIO := true
+TARGET_QCOM_AUDIO_VARIANT := caf-bfam
+AUDIO_FEATURE_DISABLED_HWDEP_CAL := true
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
+AUDIO_FEATURE_ENABLED_EXTN_FORMATS := true
+AUDIO_FEATURE_ENABLED_EXTN_POST_PROC := true
+AUDIO_FEATURE_ENABLED_FLUENCE := true
+AUDIO_FEATURE_ENABLED_HFP := true
+AUDIO_FEATURE_ENABLED_PCM_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_FLAC_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
+AUDIO_FEATURE_ENABLED_USBAUDIO := true
+AUDIO_FEATURE_ENABLED_COMPRESS_CAPTURE := true
+AUDIO_FEATURE_ENABLED_INCALL_MUSIC := true
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/iuni/u3/bluetooth
+
+# WiFi
+WLAN_MODULES:
+	mkdir -p $(KERNEL_MODULES_OUT)/pronto
+	mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
+	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+
+BOARD_EGL_CFG := device/iuni/u3/egl.cfg
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x1400000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x1400000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 29188144128
+BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+TARGET_RECOVERY_FSTAB := device/iuni/u3/rootdir/fstab.qcom
+
+TARGET_RELEASETOOLS_EXTENSIONS := device/iuni/u3
+
+# inherit from the proprietary version
+-include vendor/iuni/u3/BoardConfigVendor.mk
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# charger
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+HEALTHD_FORCE_BACKLIGHT_CONTROL := true
+HEALTHD_BACKLIGHT_ON_LEVEL := 125
+HEALTHD_ENABLE_TRICOLOR_LED := true
+
+# power hal
+TARGET_PROVIDES_POWERHAL := true
+
+# selinux
+#include device/qcom/sepolicy/sepolicy.mk
+
+#BOARD_SEPOLICY_DIRS += \
+#    device/iuni/u3/sepolicy
+
+#BOARD_SEPOLICY_UNION += \
+#    device.te \
+#    file_contexts \
+#    file.te \
+#    init_shell.te \
+#    mediaserver.te \
+#    mm-qcamerad.te \
+#    qseecomd.te \
+#    rmt_storage.te \
+#    sensors.te \
+#    system_app.te \
+#    system_server.te \
+#    time_daemon.te \
+#    thermal-engine.te \
+#    vold.te \
+#    property_contexts \
+#    property.te
+    
+# TWRP
+#TW_CUSTOM_THEME := device/iuni/u3/twres
+TW_FLASH_FROM_STORAGE := true
+TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_NO_USB_STORAGE := true
+#TW_ALWAYS_RMRF := true
+#TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+#TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_FB2PNG := true
+#TW_USE_TOOLBOX := true
+TW_NO_SCREEN_TIMEOUT := true
+TW_NO_SCREEN_BLANK := true
+TW_INCLUDE_CRYPTO := true
+#TW_TARGET_USES_QCOM_BSP := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+
+# Custom recovery
+#TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+#RECOVERY_VARIANT := twrp
+BOARD_HAS_NO_SELECT_BUTTON := true
+#TARGET_RECOVERY_INITRC := device/iuni/u3/twrp_init.rc
+#TARGET_RECOVERY_FSTAB := device/iuni/u3/recovery.fstab
+DEVICE_RESOLUTION := 1440x2560
+#BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+BOARD_TOUCH_RECOVERY := true
+BOARD_INCLUDE_FB2PNG := true
+#BOARD_CUSTOM_BOOTIMG_MK := device/iuni/u3/custombootimg.mk
